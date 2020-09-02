edbea113b0361fa1639dd0b676d370f0aa0ac20d
Divya Singh <singh.dvya05@gmail.com>
2020-09-02 10:59:45 +0545

    Updated setting

--- school/settings.py
+++ school/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from decouple import config
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
@@ -20,12 +23,12 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c&dkq2tpn)-fsyjmvm*0e$$ncf4ot_pbnr3emuo)c6o-qu0%xx'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['school-management07.herokuapp.com', '127.0.01']
 
 
 # Application definition
